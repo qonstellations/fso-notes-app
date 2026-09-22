beb4d1cf3bd0a8381f8885b2512b970f7e1b0394
qonstellations <aarush.a.pathak@gmail.com>
2026-09-23 00:48:27 +0530

    build: added configuration for vercel compatibility

--- backend/index.js
+++ backend/index.js
@@ -110,7 +110,4 @@ app.get('*any', (req, res) => {
   res.sendFile(path.join(import.meta.dirname, 'dist', 'index.html'))
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
-})
+export default app;
