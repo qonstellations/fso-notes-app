43c0de999458569a52fa002cd99fd4b9ca50c8b0
qonstellations <aarush.a.pathak@gmail.com>
2026-09-22 21:56:51 +0530

    fix: added API_URL to .env instead of hardcoding

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001/api/notes'
+const baseURL = process.env.API_URL || 'http://localhost:3001'
 
 const getAll = () => {
   return axios
